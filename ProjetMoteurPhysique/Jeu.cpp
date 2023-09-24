6baa80e8107054382482f5a340c8e61dcb788dae
Alain <alainchampigny@outlook.fr>
2023-09-24 17:11:37 -0400

    Modif classe Jeu: ajout d'une fenetre en attribut privé + getter, setter + fonction stop + modif constructeur

--- ProjetMoteurPhysique/Jeu.cpp
+++ ProjetMoteurPhysique/Jeu.cpp
@@ -1,24 +1,42 @@
 #include "Jeu.h"
+#include "OpenglImGui.h"
 
 //Constructeur
-Jeu::Jeu() {
+Jeu::Jeu(GLFWwindow* window) {
     this->setEtat(false);
+    this->setWindow(window);
 }
 
 bool Jeu::getEtat() {
     return this->etat;
 }
 
+GLFWwindow* Jeu::getWindow() {
+    return this->window;
+}
+
 void Jeu::setEtat(bool newEtat) {
     this->etat = newEtat;
 }
 
+void Jeu::setWindow(GLFWwindow* newWindow) {
+    this->window = newWindow;
+}
+
 void Jeu::start() {
     this->setEtat(true);
+    glfwMakeContextCurrent(this->getWindow());
+    const char* glsl_version = "#version 130";
+    OpenGlImGui imgui(window, glsl_version);
 }
 
 void Jeu::update() {
     if (etat) {
         //...
     }
+}
+
+void Jeu::stop() {
+    //...
+    this->setEtat(false);
 }
--- ProjetMoteurPhysique/Jeu.h
+++ ProjetMoteurPhysique/Jeu.h
@@ -1,14 +1,19 @@
 #pragma once
+#include <glfw3.h>
 class Jeu
 {
 private:
     bool etat;
+    GLFWwindow* window;
 
 public:
-    Jeu();
+    Jeu(GLFWwindow* window);
     bool getEtat();
+    GLFWwindow* getWindow();
     void setEtat(bool newEtat);
+    void setWindow(GLFWwindow* newWindow);
     void start();
     void update();
+    void stop();
 };
 
