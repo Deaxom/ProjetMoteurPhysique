a82b676503044c444165a8fb9c6e5a34f6a269b3
Alain <alainchampigny@outlook.fr>
2023-09-24 15:57:28 -0400

    Modification de la classe Jeu. Fonction update() non implémentée.

--- ProjetMoteurPhysique/Jeu.cpp
+++ ProjetMoteurPhysique/Jeu.cpp
@@ -1,1 +1,24 @@
 #include "Jeu.h"
+
+//Constructeur
+Jeu::Jeu() {
+    this->setEtat(false);
+}
+
+bool Jeu::getEtat() {
+    return this->etat;
+}
+
+void Jeu::setEtat(bool newEtat) {
+    this->etat = newEtat;
+}
+
+void Jeu::start() {
+    this->setEtat(true);
+}
+
+void Jeu::update() {
+    if (etat) {
+        //...
+    }
+}
--- ProjetMoteurPhysique/Jeu.h
+++ ProjetMoteurPhysique/Jeu.h
@@ -2,42 +2,13 @@
 class Jeu
 {
 private:
-    void GérerEntrées() {
-        // Gérer les entrées utilisateur
-    }
-
-    void MettreÀJour() {
-        // Mettre à jour la simulation (physique, intégration, etc.)
-    }
-
-    void Rendu() {
-        // Rendu graphique (OpenGL, ImGui, etc.)
-    }
-
-    bool fenetreEstOuverte = true;
-    bool fenetreDoitFermer = false;
+    bool etat;
 
 public:
-    Jeu() {
-        // Initialisation de votre jeu
-    }
-
-    void BoucleDeJeu() {
-        while (fenetreEstOuverte) {
-            // Gérer les entrées utilisateur
-            GérerEntrées();
-
-            // Mettre à jour la simulation (intégration, physique, etc.)
-            MettreÀJour();
-
-            // Rendu graphique (OpenGL, ImGui, etc.)
-            Rendu();
-
-            // Vérifier si la fenêtre doit être fermée
-            if (fenetreDoitFermer) {
-                fenetreEstOuverte = false;
-            }
-        }
-    }
+    Jeu();
+    bool getEtat();
+    void setEtat(bool newEtat);
+    void start();
+    void update();
 };
 
