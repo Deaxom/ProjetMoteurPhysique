dcb5ee004cab645d2667caede106b0d66820b6db
Alain <alainchampigny@outlook.fr>
2023-10-21 23:56:38 -0400

    Correction Flottabilite

---
+++ ProjetMoteurPhysique/ParticuleFlottabilite.cpp
@@ -0,0 +1,24 @@
+#include "ParticuleFlottabilite.h"
+
+void ParticuleFlottabilite::MiseAJourForce(Particule* particule, double deltaTime) {
+
+	//Calcule de d
+	double d = (particule->getPosition()->getY() - m_hauteurEau - m_profondeurMax) / (2 * (double)m_profondeurMax);
+
+	//On calcule la force en fonction du resultat de d
+	double force;
+
+	if (d <= 0) {
+		force = 0;
+	}
+	else if (d >= 1) {
+		force = (double)m_volume * m_densiteLiquide;
+	}
+	else {
+		force = d * m_volume * m_densiteLiquide;
+	}
+
+	//On applique la force
+	particule->getAcceleration()->setY(particule->getAcceleration()->getY() * (force / particule->getMasse()) * deltaTime);
+
+}
